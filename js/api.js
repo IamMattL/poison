f3e568a29822f9adc181b9b0208d34a47d11f804
Matt Lambert <matt.lambert@ifsworld.com>
2017-03-19 11:20:37 +0000

    ba

--- js/api.js
+++ js/api.js
@@ -1,27 +1,25 @@
 $(document).ready(function() {
     $.getJSON('http://192.168.252.66:60017/api/legit', function(data) {
 
-        $('#legit-news li').remove();
         $.each(data, function(key, newsItem) {
             var title = newsItem.Title;
             var summary = newsItem.Summary;
             var url = newsItem.Url;
             var published = newsItem.Published;
             var score = newsItem.Score;
-            $('#legit-news').append('<li>' + title + '</li>');
+            $('#legit-news').append('<article><a href="' + url + '"><img src="images/placeholder.png" alt="' + title + '" /></a><div class="content"><a href="' + url + '"><h3><img src="images/testtube-empty.svg">' + title + '</h3></a><p>' + summary + '</p></div></article>');
         });
     });
 
     $.getJSON('http://192.168.252.66:60017/api/fake', function(data) {
 
-        $('#fake-news li').remove();
         $.each(data, function(key, newsItem) {
             var title = newsItem.Title;
             var summary = newsItem.Summary;
             var url = newsItem.Url;
             var published = newsItem.Published;
             var score = newsItem.Score;
-            $('#fake-news').append('<li>' + title + '</li>');
+            $('#legit-news').append('<article><a href="' + url + '"><img src="images/placeholder.png" alt="' + title + '" /></a><div class="content"><a href="' + url + '"><h3><img src="images/testtube-full.svg">' + title + '</h3></a><p>' + summary + '</p></div></article>');
         });
     });
 });
